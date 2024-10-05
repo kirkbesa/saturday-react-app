ca71698f172f4d44e79e8bb722c078f13af3e176
CIITSHSDTP00\CIIT <kirk.besa@ciit.edu.ph>
2024-10-05 11:29:21 +0800

    to revise

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import Title from './components/Title.js';
 import Navigation from './components/Navigation.js';
 import Body from './components/Body.js';
+import Count from './pages/Count.js';
+import Calculator from './pages/Calculator.js';
 
-function App() {
+export default function App() {
   return (
     <div>
-      <div className='parent-container'>
+      <div>
+          <Title titleText='Grades Calculator'/>
+          <Calculator />
+      </div>
+      
+      {/* <div className='parent-container'>
         <Title titleText='Simple React SPA'/>
         <Navigation />
         <Body 
@@ -14,8 +21,12 @@ function App() {
           contentText='Lorem ipsum dolor sit amet, consectetur adipiscing elit. In faucibus diam vitae nunc venenatis, at placerat turpis tempus. Proin interdum gravida est, imperdiet luctus eros feugiat id. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In eu ante non velit ultrices hendrerit. Donec sollicitudin tellus nec enim molestie, nec molestie magna elementum. Etiam iaculis, neque et pretium iaculis, justo tellus convallis urna, sit amet imperdiet sem dui eget massa. Nunc volutpat justo id pretium fermentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ac orci ligula.'
         />
       </div>
+
+      <div className='parent-container'>
+        <Title titleText='Count'/>
+          <Count />
+      </div> */}
+
     </div>
   );
 }
-
-export default App;
