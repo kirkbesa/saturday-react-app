5934d15bba86756bd15257f736279d2e153fa8fa
CIITSHSDTP00\CIIT <kirk.besa@ciit.edu.ph>
2024-10-19 10:59:01 +0800

    Routing

---
+++ src/pages/About.js
@@ -0,0 +1,10 @@
+import './About.css';
+
+export default function About() {
+  return (
+    <div className="parent-container">
+      <h1>About</h1>
+      <p>Hi , I'm a paragraph about the about page.</p>
+    </div>
+  );
+}
