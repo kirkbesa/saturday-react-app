2f8f936b505d22240f023f132df9ef9a429c5069
Kirk Besa <kirkpatrickbesa@gmail.com>
2024-10-19 10:59:01 +0800

    Routing

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,22 @@
 import './Navigation.css';
+import  { NavLink } from 'react-router-dom';
+import App  from '../App.js';
+
+
 
 export default function Navigation() {
 
     return (
         <div>
             <nav>
-                <ul>
-                    <li className='active'>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                </ul>
+                <div className="left">
+                    <h1>My Page</h1>
+                </div>
+                <div className="right">
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                </div>
             </nav>
         </div>
     );
